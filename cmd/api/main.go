c9493a660f5a12900e08e54a34b05ffb020aec8b
Aryan Mishra <mishraaryan9204@gmail.com>
2025-08-30 19:30:21 +0530

    Migrations and make file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/AryanMishra09/Social/internal/db"
 	"github.com/AryanMishra09/Social/internal/env"
 	"github.com/AryanMishra09/Social/internal/store"
 	"github.com/joho/godotenv"
@@ -18,9 +19,28 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8000"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost:5232/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Printf("DB Connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
